dbcd2f6d8faf5f662e877f9d1a94262e1aba8959
Ronil Doshi <ronildoshi@my.unt.edu>
2024-10-22 16:03:20 -0500

    End of Section 3

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -10,7 +10,7 @@ public class Product : BaseEntity
 
     public decimal Price { get; set; }
 
-    public required string PictureURL {get; set; }
+    public string PictureURL {get; set; } = "default.jpeg";
 
     public required string Type { get; set; }
 
